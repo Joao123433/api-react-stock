55fd4eb43ef4d3a29572719e2e89369b521050c3
Joao123433 <joaosantos123433@gmail.com>
2024-06-30 00:12:56 -0300

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-// const cors = require("cors");
+const cors = require("cors");
 require("./database/config")
 
 const categoryRouter = require("./src/routes/category")
@@ -8,7 +8,7 @@ const ItemRouter = require("./src/routes/item")
 const app = express()
 
 app.use(express.json())
-// app.use(cors());
+app.use(cors());
 
 app.use("/api/category", categoryRouter)
 app.use("/api/item", ItemRouter)
