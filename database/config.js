6a8eb72c2164ba70a6ffd02fa47c992c922749f0
Joao123433 <joaosantos123433@gmail.com>
2024-06-29 22:30:31 -0300

    Ajuste vercel.json

--- database/config.js
+++ database/config.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose")
 
 mongoose.Promise = global.Promise
 
-mongoose.connect("mongodb+srv://joaosantos123433:Feliznatal2024!@cluster01.ahojeyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01")
-  .then(() => console.log("Conectado com sucesso ao mongodb"))
-  .catch((error) => console.log(error))
+mongoose.connect("mongodb+srv://joaosantos123433:Feliznatal2024!@cluster01.ahojeyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log("Conectado com sucesso ao mongodb"))
+    .catch((error) => console.log(error))
